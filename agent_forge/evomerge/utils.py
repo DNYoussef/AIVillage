524f0f6f31fa5418dded63fce41f4cc207297f18
DNYoussef <dydavidyoussef@gmail.com>
2024-10-10 19:10:33 -0400

    make comits

--- agent_forge/evomerge/utils.py
+++ agent_forge/evomerge/utils.py
@@ -61,7 +61,6 @@ def load_single_model(model_ref: ModelReference) -> torch.nn.Module:
         model = AutoModelForCausalLM.from_pretrained(
             model_ref.path,
             device_map="auto",
-            load_in_8bit=True,
             torch_dtype=torch.float16,
             low_cpu_mem_usage=True
         )
@@ -79,7 +78,7 @@ def load_model_process(model_ref, queue):
     except Exception as e:
         queue.put(("error", str(e), traceback.format_exc()))
 
-def load_model_with_timeout(model_ref: ModelReference, timeout: int = 1200) -> torch.nn.Module:
+def load_model_with_timeout(model_ref: ModelReference, timeout: int = 3600) -> torch.nn.Module:
     queue = multiprocessing.Queue()
     process = multiprocessing.Process(target=load_model_process, args=(model_ref, queue))
     process.start()
@@ -298,7 +297,3 @@ MERGE_TECHNIQUES: Dict[str, Callable] = {
     "frankenmerge": frankenmerge,
     "dfs": dfs_merge
 }
-
-
-
-
