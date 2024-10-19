6f5220440858e4c0a74e84bed57562b618ea0f31
DNYoussef <dydavidyoussef@gmail.com>
2024-10-19 12:31:57 -0400

    Update pipeline.py to use UnifiedConfig

--- rag_system/core/pipeline.py
+++ rag_system/core/pipeline.py
@@ -1,17 +1,17 @@
 from typing import Dict, Any
-from rag_system.core.config import UnifiedConfig
+from rag_system.core.unified_config import UnifiedConfig
 from rag_system.core.latent_space_activation import LatentSpaceActivation
 from rag_system.retrieval.hybrid_retriever import HybridRetriever
 from rag_system.processing.reasoning_engine import UncertaintyAwareReasoningEngine
 from rag_system.core.cognitive_nexus import CognitiveNexus
 
 class EnhancedRAGPipeline:
-    def __init__(self, config: UnifiedConfig):
-        self.config = config
-        self.latent_space_activation = LatentSpaceActivation()
-        self.hybrid_retriever = HybridRetriever(config)
-        self.reasoning_engine = UncertaintyAwareReasoningEngine(config)
-        self.cognitive_nexus = CognitiveNexus()
+    def __init__(self):
+        self.config = UnifiedConfig()
+        self.latent_space_activation = LatentSpaceActivation(self.config)
+        self.hybrid_retriever = HybridRetriever(self.config)
+        self.reasoning_engine = UncertaintyAwareReasoningEngine(self.config)
+        self.cognitive_nexus = CognitiveNexus(self.config)
 
     async def process_query(self, query: str, **kwargs) -> Dict[str, Any]:
         # Latent space activation
